2c162b3ffcc888f614a6e17a3e90b01dac17fe3a
Kevin Zhang <kzhang@federatedwireless.com>
2017-09-04 17:38:43 -0400

    basic store for now...

---
+++ src/configureStore.js
@@ -0,0 +1,9 @@
+import {createStore} from 'redux';
+import rootReducer from './reducers';
+
+export default function configureStore(preloadedState) {
+	return createStore(
+		rootReducer,
+		preloadedState
+	);
+}
