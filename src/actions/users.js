e01364b40fe435322301d39ac4732d0014b96e51
Kevin Zhang <kzhang@federatedwireless.com>
2017-09-04 20:08:09 -0400

    users actions and reducers

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,1 +1,33 @@
+export const USERS_AUTHENTICATE = "USERS_AUTHENTICATE";
+export const USERS_LOGIN = "USERS_LOGIN";
+export const USERS_LOGOUT = "USERS_LOGOUT";
 export const USERS_REGISTER = "USERS_REGISTER";
+
+export function authenticate(username, password) {
+	return {
+		type: USERS_AUTHENTICATE,
+		username,
+		password
+	};
+}
+
+export function login() {
+	return {
+		type: USERS_LOGIN
+	};
+}
+
+export function logout() {
+	return {
+		type: USERS_LOGOUT
+	};
+}
+
+export function register(username, email, password) {
+	return {
+		type: USERS_REGISTER,
+		username,
+		email,
+		password
+	};
+}
---
+++ src/reducers/users.js
@@ -0,0 +1,6 @@
+import {
+	USERS_AUTHENTICATE,
+	USERS_LOGIN,
+	USERS_LOGOUT,
+	USERS_REGISTER
+} from "../actions/users";
