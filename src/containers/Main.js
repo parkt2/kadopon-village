69fde4c39fc2aefdb6121de5874a25b51f049493
Kevin Zhang <kzhang@federatedwireless.com>
2017-11-06 16:31:05 -0500

    eslint fix

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,8 +1,8 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
 class Main extends Component {
 	render() {
-		return(
+		return (
 			<p>main page</p>
 		);
 	}
