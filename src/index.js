1f6b841f42fae3bfdd93a8d92f02805617546d7d
Kevin Zhang <kzhang@federatedwireless.com>
2018-02-15 14:39:15 -0500

    #7 | initial mongoose code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import mongoose from "mongoose";
 
 import configureStore from "./configureStore";
 import Main from "./containers/Main";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
+
+mongoose.connect("localhost:27017/kadopon");
 
 const store = configureStore();
 
