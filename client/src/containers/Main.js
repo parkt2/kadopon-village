53ae5d48e98614139c9a3c6754c1287eb771b8f8
Kevin Zhang <kzhang@federatedwireless.com>
2018-02-16 10:01:41 -0500

    #7 | commit express changes

--- client/src/containers/Main.js
+++ client/src/containers/Main.js
@@ -11,6 +11,29 @@ import { connect } from "react-redux";
 import Error404 from "../components/errors/Error404";
 
 class Main extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			response: "",
+		};
+	}
+
+	componentDidMount() {
+		this.callApi()
+			.then(res => this.setState({ response: res.Express }))
+			.catch(err => console.log(err));
+	}
+
+	callApi = async () => {
+		const response = await fetch("/api/hello");
+		const body = await response.json();
+
+		if (response.status !== 200) throw Error(body.message);
+
+		return body;
+	}
+
 	// some function here to check if a user is authenticated
 	renderRoutes = routes => routes.map(route => (
 		<Route
@@ -34,6 +57,7 @@ class Main extends Component {
 
 		return (
 			<div>
+				<p>{this.state.response}</p>
 				<Router history={createHistory()}>
 					<div>
 						<Switch>
@@ -50,7 +74,7 @@ class Main extends Component {
 	}
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = () => {
 	// this is where you get the state from reducers and
 	// pass them to this component as props
 	// this is useful for getting the authentication status of a
--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import mongoose from "mongoose";
 
 import configureStore from "./configureStore";
 import Main from "./containers/Main";
 import registerServiceWorker from "./registerServiceWorker";
 import "./index.css";
-import {
-	MONGO_CONNECT,
-} from "./config";
-
-mongoose.connect(MONGO_CONNECT);
 
 const store = configureStore();
 
