c5784418a513026cad00b6e9d476091aee288134
Kevin Zhang <kzhang@federatedwireless.com>
2018-02-16 09:19:44 -0500

    add a config for mongo stuff

---
+++ src/config.template.js
@@ -0,0 +1,6 @@
+// this is a template file. changing this does not actually affect development.
+
+// mongo
+export const MONGO_SERVER = "localhost";
+export const MONGO_PORT = "27017";
+export const MONGO_DB = "kadopon";
