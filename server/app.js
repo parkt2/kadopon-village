0fc14c4e30fde579ab32a7c3a65e77b45cf2ba1d
Kevin Zhang <kzhang@federatedwireless.com>
2018-02-16 11:35:14 -0500

    #7 | use mongoose

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+
+import {MONGO_CONNECT} from "../config";
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+// mongoose setup
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_CONNECT);
+
 app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
+  res.send({ express: "hello hello" });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
