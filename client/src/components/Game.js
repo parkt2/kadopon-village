3ac33f1a6cd27e7772ac6c11880a75756873e40b
Kevin Zhang <kzhang@federatedwireless.com>
2018-03-28 15:30:28 -0400

    update game for phaser hook

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -3,9 +3,15 @@ import React, { Component } from "react";
 export default class Game extends Component {
 	render() {
 		return (
-			<div>
-				<p>XD</p>
-			</div>
+			<div
+				id="content"
+				style={{
+					backgroundColor: "black",
+					display: "block",
+					width: "100%",
+					textAlign: "center",
+				}}
+			>game</div>
 		);
 	}
 }
