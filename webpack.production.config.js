fec1c5ddea1cf19c93db69f4799d5044537cdbeb
Kevin Zhang <kzhaaang@gmail.com>
2018-05-22 23:46:08 -0400

    closes #48 | cleans up demo production build

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 const webpack = require("webpack");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 // Phaser webpack config
 const phaserModule = path.join(__dirname, "/node_modules/phaser/");
